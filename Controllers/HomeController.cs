f7cc46f5335a3bd0dbf7d5f858c3c42857ec433a
Lukasz Jedrak <slipkornletsplay@gmail.com>
2018-10-30 12:06:58 +0000

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitConverterPlus.Models;
+
+namespace UnitConverterPlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ConvertNumbers(string text, string action)
+        {
+            var converter = new UnitConverter();
+
+            if (action == "Yards to meters")
+            {       
+                return Content(converter.Converter(text, "yards").ToString());
+            }
+
+            else if (action == "Meters to yards")
+            {
+                return Content(converter.Converter(text, "meters").ToString());
+            }
+
+            else if (action == "Centimeters to inches")
+            {
+                return Content(converter.Converter(text, "centimeters").ToString());
+            }
+
+            else if (action == "Miles to kilometers")
+            {
+                return Content(converter.Converter(text, "miles").ToString());
+            }
+
+            else if (action == "Kilometers to miles")
+            {
+                return Content(converter.Converter(text, "kilometers").ToString());
+            }
+
+            else
+            {
+                return Content(converter.Converter(text, "inches").ToString());
+            }
+
+        }
+        
+    }
+}
---
+++ UnitConverter.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitConverterPlus
+{
+    public class UnitConverter
+    {
+        List<string> resultAfterConvert = new List<string>();
+        List<string> multiValuesWithDot = new List<string>();
+        public bool exception = false;
+
+        public string Converter(string convert, string type)
+        {
+            if (convert == null)
+            {
+                string exception = "Please enter some values.";
+                return exception;
+            }
+
+            else
+            {
+                if (convert.Contains(" "))
+                {
+                    List<string> listOfValues = MultipleValuesWithDot(convert);
+                    MultipleValuesCalculator(type, listOfValues);
+
+                    if (exception == true)
+                    {
+                        string exception = "Please enter a real value!";
+                        return exception;
+                    }
+
+                    else
+                    {
+                        var stringResult = string.Join(Environment.NewLine, resultAfterConvert.ToList());
+                        return stringResult;
+                    }
+                    
+                }
+
+                else
+                {
+                    convert = SingleValueWithDot(convert);
+                    return SingleValuesCalculator(convert, type);
+                }    
+
+            }
+
+        }
+
+        private string SingleValuesCalculator(string convert, string type)
+        {
+            try
+            {
+                if (type == "yards")
+                {
+                    return Math.Round(double.Parse(convert) * 0.9144, 10).ToString();
+                }
+
+                else if (type == "meters")
+                {
+                    return Math.Round(double.Parse(convert) * 1.0936132983, 10).ToString();
+                }
+
+                else if (type == "inches")
+                {
+                    return Math.Round(double.Parse(convert) * 2.54, 10).ToString();
+                }
+
+                else if (type == "centimeters")
+                {
+                    return Math.Round(double.Parse(convert) / 2.54, 10).ToString();
+                }
+
+                else if (type == "miles")
+                {
+                    return Math.Round(double.Parse(convert) * 1.60934, 10).ToString();
+                }
+
+                else
+                {
+                    return Math.Round(double.Parse(convert) / 1.60934, 10).ToString();
+                }
+            }
+            catch (FormatException ex)
+            {
+                string exception = "Please enter a real value!";
+                return exception;
+            }
+        }
+
+        private string MultipleValuesCalculator(string type, List<string> listOfValues)
+        {
+            for (int i = 0; i < listOfValues.Count; i++)
+            {
+                if (resultAfterConvert.Count > listOfValues.Count) break;
+
+                try
+                {
+                    if (type == "yards")
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) * 0.9144, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+
+                    else if (type == "meters")
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) * 1.0936132983, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+
+                    else if (type == "inches")
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) * 2.54, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+
+                    else if (type == "centimeters")
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) / 2.54, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+
+                    else if (type == "miles")
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) * 1.60934, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+
+                    else
+                    {
+                        var result = Math.Round(double.Parse(listOfValues[i]) / 1.60934, 10);
+                        resultAfterConvert.Add(result.ToString());
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    exception = true;
+                }
+
+            }
+
+            return resultAfterConvert.ToString();
+        }
+
+        private List<string> MultipleValuesWithDot(string convert)
+        {
+            var listOfValues = new List<string>();
+
+            for (int i = 0; i < convert.Split(" ").Count(); i++)
+            {
+                listOfValues.Add(convert.Split(" ")[i]);
+            }
+
+            for (int i = 0; i < listOfValues.Count; i++)
+            {
+                if (listOfValues[i].Contains(".") || listOfValues[i].Contains(","))
+                {
+                    var result = listOfValues[i].Replace('.', ',');
+                    multiValuesWithDot.Add(result);
+                    continue;
+                }
+
+                else
+                {
+                    multiValuesWithDot.Add(listOfValues[i]);
+                    continue;
+                }
+
+            }
+
+            return multiValuesWithDot;
+        }
+
+        private string SingleValueWithDot(string convert)
+        {
+            if (convert.Contains("."))
+            {
+                var replace = convert.Replace('.', ',');
+                return replace;
+            }
+
+            else
+            {
+                return convert;
+            }
+
+        }
+
+    }
+}
